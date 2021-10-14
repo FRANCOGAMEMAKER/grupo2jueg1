9bad2254bc2a18d57e83bfb67380f09e379a7905
megaketchup <megaketchup@hotmail.com>
2021-10-14 00:42:28 -0500

    Update PlayerMove.cs

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
+
 public class PlayerMove : MonoBehaviour
 {
-    public float speed;
-    public Rigidbody2D Rb2d;
+    public float speed,ddge_speed=1,ddge_dist=1,ddge_curr_dist;
+    public Rigidbody2D Rb2d;bool ddge=false;
+    Vector2 og_ddpos,target_ddpos;
+
+
 
     void Start()
     {
@@ -15,6 +22,19 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ddge_curr_dist=0;
+            ddge=true;
+           og_ddpos=Rb2d.transform.position;
+           target_ddpos=og_ddpos+new Vector2(ddge_dist,ddge_dist);
+        }
+        if (ddge&&ddge_curr_dist<1)
+        {
+            ddge_curr_dist+=Time.deltaTime*ddge_speed;
+            Rb2d.transform.position= new Vector2(Mathf.Lerp(og_ddpos.x,target_ddpos.x,ddge_curr_dist),Mathf.Lerp(og_ddpos.y,target_ddpos.y,ddge_curr_dist)) ;
+        }
+        else{ddge=false;}
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
