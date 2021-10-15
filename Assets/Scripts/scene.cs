de34d60efdb8ed85230801674998c07521b42717
megaketchup <megaketchup@hotmail.com>
2021-10-14 19:23:48 -0500

    intr vid

---
+++ Assets/Scripts/scene.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene : MonoBehaviour
+{
+    public int sceneind;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (sceneind)
+        {
+            
+            case 0:Invoke("changetogame",14);
+            break;
+            case 1://Invoke();
+            break;
+            case 2://Invoke();
+            break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void changetogame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    
+}
